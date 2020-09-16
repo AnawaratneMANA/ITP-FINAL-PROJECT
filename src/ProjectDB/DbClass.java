5b3094322c26a9573c0e0da81d52d1ce7c207206
SalithaEkanayaka123 <ekanayakasalitha1@gmail.com>
2020-09-16 07:48:43 +0530

    Added a comment to the DB class

--- src/ProjectDB/DbClass.java
+++ src/ProjectDB/DbClass.java
@@ -24,6 +24,7 @@ public class DbClass {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/itpfinaldb","root","password");
+            //When clone the project, Make sure to change the username and password accroding to your server.
             return con;
         } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DbClass.class.getName()).log(Level.SEVERE, null, ex);
